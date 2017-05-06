ef11d2fdf7b5e7f9e25a426f852de95511166172
mesqfel <mesquita.superlogica@gmail.com>
2017-05-05 21:10:23 -0300

    Hidding my api key

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_KEY = '8cf59b9fde73d3cfa8a6a306774963ba'
+const API_KEY = 'YOUR_OPENWEATHERMAP_API_KEY_HERE'
 const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?units=metric&appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
